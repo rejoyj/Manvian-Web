e8e31bed5375c7afaa2cf148bba88792e7aec314
Rejoy J <rrejoy7@gmail.com>
2025-07-01 16:32:15 +0530

    4 component Done

---
+++ Manvian/src/Component/RightBottom.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./RightBottom.css";
+
+const RightBottom = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="container">
+          <p>
+            To be a global force for good - To inspire a world where businesses
+            are built not just to succeed, but to serve by redefining the way
+            the world does business by building ventures that focus on people ,
+            planet and purpose by creating lasting value. (We envision a world
+            where growth is not just measured by numbers, but by the value we
+            create for communities, the opportunities we unlock for individuals,
+            and the positive impact we leave on the planet.)
+          </p>
+          <img src="\src\assets\image.png" alt="" width={669} height={636} />
+        </div>
+        <div className="blue-box">
+          <img
+            src="\src\assets\bluebox.png "
+            alt=""
+            width={300}
+            height={300}
+            className="two"
+          />
+          <strong>
+            <h1>Our Vision</h1>
+          </strong>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RightBottom;
